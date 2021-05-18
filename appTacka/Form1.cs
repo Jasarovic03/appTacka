1e1aede27f43c9943559d0328a4314cd22666038
Medžid Jašarović <jm709@eprosveta.rs>
2021-05-18 11:41:58 +0200

    Add project files.

---
+++ appTacka/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appTacka
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Tacka[] t = new Tacka[30];
+        int n = 30;
+        Random r = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0;i<n;i++)
+            {
+                int x = r.Next(ClientRectangle.Width);
+                int y = r.Next(ClientRectangle.Height);
+                t[i] = new Tacka(x, y);
+
+            }
+            timer1.Interval = 500;
+            timer1.Start();
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                t[i].Crtaj(e.Graphics);
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (n > 1)
+            {
+                t[n - 2] = t[n - 2] + t[n - 1];
+                n--;
+                Refresh();
+            }
+         
+        }
+    }
+}
---
+++ appTacka/Tacka.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace appTacka
+{
+    class Tacka
+    {
+        private int x, y;
+        public int X
+        {
+            set
+            {
+                if (value < 0) throw new Exception("x manje od 0");
+                else x = value;
+                
+            }
+            get { return x; }
+
+        }
+        public int Y
+        {
+            set
+            {
+                if (value < 0) throw new Exception("y manje od nula");
+
+            }
+            get { return y; }
+        }
+        public Tacka(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Crtaj(Graphics g)
+        {
+            g.FillEllipse(Brushes.Red, x - 2, y - 2, 4, 4);
+        }
+        public static Tacka operator+(Tacka t1,Tacka t2)
+        {
+            int x = (t1.x + t2.x) / 2;
+            int y = (t1.y + t2.y) / 2;
+            return new Tacka(x, y);
+        }
+    }
+}
